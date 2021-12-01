30899ebdc3e4686e6698f9e5f769a931f33d112d
anish wagle <anish.wagle@bhattaraisoftwareinc.com>
2021-12-01 10:09:43 +0545

    resolved the issue of local host

--- Program.cs
+++ Program.cs
@@ -20,7 +20,9 @@ namespace FND
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder
+                    .UseStartup<Startup>();
                 });
+
     }
 }
