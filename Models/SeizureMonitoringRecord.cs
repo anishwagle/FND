0cc5d0756bf485c21a28ce617f4f499db5b4e55d
anish wagle <anish.wagle@bhattaraisoftwareinc.com>
2021-12-11 04:25:35 +0000

    updated the model

--- Models/SeizureMonitoringRecord.cs
+++ Models/SeizureMonitoringRecord.cs
@@ -11,6 +11,20 @@ namespace FND.Models
         [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
         public User User { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public DateTime DateOfPlan { get; set; }
+        public bool OnMedication { get; set; }
+        public string Medication { get; set; }
+        public List<string> MedicalConditions { get; set; }
+        public string MedicalHistory { get; set; }
+        public Contact EmergencyContact { get; set; }
+        public Contact DoctorContact { get; set; }
+        public string WarningSign { get; set; }
+        public string SeizureType { get; set; }
+        public string PresentSeizureCondition { get; set; }
+        public string SeizureDuration { get; set; }
+        public string Frequenty { get; set; }
+        public string FrequencyUnit { get; set; }
         public string AssistanceRequiredFromPeople { get; set; }
         public string UnwantedSeizureThings { get; set; }
         public string NeededAfterSeizure { get; set; }
@@ -18,5 +32,13 @@ namespace FND.Models
         public string PatientName { get; set; }
         public string EmergencyContact { get; set; }
         public string RelationshipToPatient { get; set; }
+    }
+    public class Contact{
+        public string Name { get; set; }
+        public string RelationShip{ get; set; }
+        public string Phone { get; set; }
+        public string HomePhone { get; set; }
+        public string Address { get; set; }
+        public string Profession { get; set; }
     }
 }
