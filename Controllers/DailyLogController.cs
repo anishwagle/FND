583ba822cbcc382dd924821e7769b10180551c23
anish wagle <anish.wagle@bhattaraisoftwareinc.com>
2021-12-13 16:41:54 +0000

    controller for daily log added

---
+++ Controllers/DailyLogController.cs
@@ -0,0 +1,52 @@
+using System.Threading.Tasks;
+using FND.DTO;
+using FND.Models;
+using FND.Services;
+using FND.Middlewares;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FND.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DailyLogController : BaseApiController
+    {
+        private readonly IDailyLogService _dailyLogService;
+        public DailyLogController(IDailyLogService dailyLogService)
+        {
+            _dailyLogService = dailyLogService;
+        }
+        [Authorize]
+        [HttpPost]
+        [Route("add")]
+        public async Task<IActionResult> AddAsync(DailyLogRequest model)
+        {
+            var response = await _dailyLogService.AddAsync(model);
+            return Ok(CreateSuccessResponse(response));
+        }
+        [Authorize]
+        [HttpPost]
+        [Route("get-all/{userId}")]
+        public async Task<IActionResult> GetAsync([FromRoute] string userId)
+        {
+            var response = await _dailyLogService.GetDailyLogsAsync(userId);
+            return Ok(CreateSuccessResponse(response));
+        }
+        [Authorize]
+        [HttpPost]
+        [Route("get-by-id/{userId}/{id}")]
+        public async Task<IActionResult> GetAsync([FromRoute] string userId, [FromRoute] string Id)
+        {
+            var response = await _dailyLogService.GetByIdAsync(userId, Id);
+            return Ok(CreateSuccessResponse(response));
+        }
+        [Authorize]
+        [HttpPost]
+        [Route("delete/{id}")]
+        public IActionResult Delete([FromRoute] string Id)
+        {
+            _dailyLogService.Delete(Id);
+            return Ok(CreateSuccessResponse("Deleted"));
+        }
+    }
+}
