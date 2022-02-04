d5fe6ad219a917db6c1e26ef87fee611c2b0c79c
anish wagle <anish.wagle@bhattaraisoftwareinc.com>
2022-02-04 15:50:17 +0545

    updated smpform

--- Models/SMPForm.cs
+++ Models/SMPForm.cs
@@ -6,32 +6,32 @@ using System.Collections.Generic;
 namespace FND.Models
 {
     public class SMPForm{
-        [BsonId]
+          [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
         public User User { get; set; }
         public DateTime DateOfBirth { get; set; }
         public DateTime DateOfPlan { get; set; }
-        public string onMedication { get; set; }
+        public bool OnMedication { get; set; }
         public string Medication { get; set; }
-        public List<string> MedicalCondition { get; set; }
+        public List<string> MedicalConditions { get; set; }
         public string MedicalHistory { get; set; }
-        public List<string> WarningSign { get; set; }
-        public string WarningSignText { get; set; }
-        public List<string> SeizureType { get; set; }
+        public List<string> WarningSigns { get; set; }
+        public string WarningSignsText { get; set; }
+        public string SeizureType { get; set; }
         public string SeizureTypeText { get; set; }
         public List<string> SeizurePresent { get; set; }
         public string SeizurePresentText { get; set; }
-
         public string DurationOfSeizure { get; set; }
-        public string FrequencyOfSeizure { get; set; }
+        public string Frequency { get; set; }
+        public string FrequencyUnit { get; set; }
         public List<string> AssistanceRequired { get; set; }
         public string AssistanceRequiredText { get; set; }
         public List<string> NotDo { get; set; }
         public string NotDoText { get; set; }
-        public List<string> NeedAfterSeizure { get; set; }
+        public string NeedAfterSeizure { get; set; }
         public string NeedAfterSeizureText { get; set; }
-        public List<string> AmbulanceNeeded { get; set; }
-        public string AmbulanceNeededText { get; set; }
+        public List<string> ambulanceNeeded { get; set; }
+        public string ambulanceNeededText { get; set; }
     }
 }
